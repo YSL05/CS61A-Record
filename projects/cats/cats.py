9485141eff704d910677e5a421818deecd5c3ade
YSL05 <271367835@qq.com>
2024-08-12 00:03:11 +0800

    add project cats with func wpm

--- projects/cats/cats.py
+++ projects/cats/cats.py
@@ -133,6 +133,7 @@ def wpm(typed, elapsed):
     assert elapsed > 0, "Elapsed time must be positive"
     # BEGIN PROBLEM 4
     "*** YOUR CODE HERE ***"
+    return float((len(typed) / 5) * (60 / elapsed))
     # END PROBLEM 4
 
 
